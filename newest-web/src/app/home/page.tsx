1228fa542db9ae8f999e38f17524b12ce9ec8917
shin950924 <50609466+shin950924@users.noreply.github.com>
2025-06-22 19:07:36 +0900

    수정

--- newest-web/src/app/home/page.tsx
+++ newest-web/src/app/home/page.tsx
@@ -9,10 +9,10 @@ export const metadata: Metadata = {
     title: "Newest - 실시간 급상승 검색어",
     description: "당신만 모르는 실시간 검색어! 지금 확인해 보세요!",
     url: "https://www.shin1995seoul.com/",
-    siteName: "MyApp",
+    siteName: "Newest - 실시간 급상승 검색어",
     images: [
       {
-        url: "/images/001.png",
+        url: "https://www.shin1995seoul.com/images/001.png",
         width: 1200,
         height: 630,
         alt: "Newest - 실시간 급상승 검색어",
